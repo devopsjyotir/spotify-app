d6f480a7fae7e6d9ef24af3aaf50c119477fc996
devopsjyotir <jyotirjy@gmail.com>
2023-01-29 00:10:51 -0500

    set up route to refress access token

--- index.js
+++ index.js
@@ -114,6 +114,37 @@ res.send(response)
    
 })
 
+//route to refresh access token 
+app.get('/refresh_token', (req, res) => {
+    const { refresh_token} = req.query
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        //data object formats the three required body params using stringify
+        data: querystring.stringify({
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        }),
+    
+    //setup two reauest headers
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+        }
+    
+    })
+    
+    .then(response => {
+       
+    res.send(response.data)
+        }
+    )
+    
+    .catch(error => {
+        res.send(error)
+    })
+})
 
 app.listen(port, ()=> {
     console.log(`Express app listening at http://localhost:${port}`)
