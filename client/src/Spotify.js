16afac16bb2837ca01e21d501d9efc232b6c3093
devopsjyotir <jyotirjy@gmail.com>
2023-01-31 14:32:59 -0500

    implemented local storage keys and local storage values

--- client/src/Spotify.js
+++ client/src/Spotify.js
@@ -1,3 +1,21 @@
+
+//Map for localstorage keys
+const LOCALSTORAGE_KEYS = {
+    accessToken: 'spotify_access_token',
+    refreshToken: 'spotify_refresh_token',
+    expireTime: 'spotify_token_expire_time',
+    timestamp: 'spotify_token_timestamp'
+}
+
+//MAp to retrieve localstorage values
+const LOCALSTORAGE_VALUES = {
+        access_token: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+        refreshtoken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+        expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+        timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+
+}
+
 const getAccessToken = () => {
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
